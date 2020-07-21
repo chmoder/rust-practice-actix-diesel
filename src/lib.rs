4e750b36a319e042c6fbce7c6d00d8ea3b740a41
tcross <tom.bz2@gmail.com>
2020-07-20 21:01:43 -0500

    minor cleanup and error fixes

--- src/lib.rs
+++ src/lib.rs
@@ -19,7 +19,8 @@ pub fn establish_connection() -> PgConnection {
     dotenv().ok();
 
     let database_url = env::var("DATABASE_URL")
-        .expect("DATABASE_URL must be set");
+        .expect("DATABASE_URL must be set in .env file");
+
     PgConnection::establish(&database_url)
         .expect(&format!("Error connecting to {}", database_url))
 }
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
 use actix_web::{web, get, post, App, Responder, HttpServer, error, HttpRequest, HttpResponse};
 use deadpool_redis::{Config, Pool};
 use deadpool_redis::redis::AsyncCommands;
-use rust_practice_actix_web::{establish_connection, get_posts, create_post, create_zip_code, create_med_supp, search_med_supp};
 use diesel::PgConnection;
 use actix_web::web::Json;
-use rust_practice_actix_web::models::{NewZipCode, NewMedSupp, MedSupp, ZipCode};
 use chrono::NaiveDateTime;
+use serde::Deserialize;
+use rust_practice_actix_diesel::models::{NewZipCode, NewMedSupp};
+use rust_practice_actix_diesel::{create_post, get_posts, create_zip_code, create_med_supp, search_med_supp, establish_connection};
+
 
 fn json_error_handler(err: error::JsonPayloadError, _req: &HttpRequest) -> error::Error {
     let detail = err.to_string();
@@ -18,47 +20,25 @@ fn json_error_handler(err: error::JsonPayloadError, _req: &HttpRequest) -> error
     error::InternalError::from_response(err, response).into()
 }
 
-
-async fn get_from_redis(key: String, redis_pool: web::Data<Pool>) -> String {
-    let mut conn = redis_pool.get().await.unwrap();
-    return conn.get(key).await.unwrap_or_default();
-}
-
-async fn add_to_redis(key: String, val: String, redis_pool: web::Data<Pool>) {
-    let mut conn = redis_pool.get().await.unwrap();
-    let _:() = conn.set(key, val).await.unwrap_or_default();
-}
-
 #[get("/{id}/{name}/index.html")]
-async fn index(path_params: web::Path<(String, u32)>, redis_pool: web::Data<Pool>, pg_connection: web::Data<PgConnection>) -> impl Responder {
-    // let mut item= get_from_redis(path_params.0.clone(), redis_pool.clone()).await;
-    //
-    // if item.is_empty() {
-    //     add_to_redis(path_params.0.clone(), "42".to_string(), redis_pool.clone()).await;
-    //     item = get_from_redis(path_params.0.clone(), redis_pool.clone()).await;
-    // }
-
+async fn index(path_params: web::Path<(String, u32)>, pg_connection: web::Data<PgConnection>) -> impl Responder {
     create_post(&pg_connection, "post title 1", "post body 1");
     let posts = get_posts(&pg_connection);
     Json(posts)
-
-    // Ok(Json(posts))
-    // format!("Hello {}! id:{} redis_value:{}", path_params.0, path_params.1, item)
 }
 
 #[post("/zip-codes")]
-async fn create_zip_code_handler(new_item: web::Json<NewZipCode>, redis_pool: web::Data<Pool>, pg_connection: web::Data<PgConnection>) -> impl Responder {
+async fn create_zip_code_handler(new_item: web::Json<NewZipCode>, pg_connection: web::Data<PgConnection>) -> impl Responder {
     let zip_code = create_zip_code(&pg_connection, &new_item);
     Json(zip_code)
 }
 
 #[post("/med-supp")]
-async fn create_med_supp_handler(new_item: web::Json<NewMedSupp>, redis_pool: web::Data<Pool>, pg_connection: web::Data<PgConnection>) -> impl Responder {
+async fn create_med_supp_handler(new_item: web::Json<NewMedSupp>, pg_connection: web::Data<PgConnection>) -> impl Responder {
     let med_supp = create_med_supp(&pg_connection, &new_item);
     Json(med_supp)
 }
 
-use serde::Deserialize;
 #[derive(Deserialize)]
 pub struct MedSuppQuoteRequest {
     zip5: String,
@@ -69,7 +49,7 @@ pub struct MedSuppQuoteRequest {
 }
 
 #[get("/med-supp")]
-async fn med_supp_quote_list(web::Query(info): web::Query<MedSuppQuoteRequest>, redis_pool: web::Data<Pool>, pg_connection: web::Data<PgConnection>) -> impl Responder {
+async fn med_supp_quote_list(web::Query(info): web::Query<MedSuppQuoteRequest>, pg_connection: web::Data<PgConnection>) -> impl Responder {
 
     let thing = search_med_supp(
         &pg_connection,
@@ -85,13 +65,7 @@ async fn med_supp_quote_list(web::Query(info): web::Query<MedSuppQuoteRequest>, 
 
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
-    let cfg = Config::from_env("prefix").unwrap();
-    let pool = cfg.create_pool().unwrap();
-
-    let pg_connection = establish_connection();
-
     HttpServer::new(move || App::new()
-        .data(pool.clone())
         .data(establish_connection())
         .service(
         index
@@ -106,7 +80,7 @@ async fn main() -> std::io::Result<()> {
         )
     )
         .workers(num_cpus::get())
-        .bind("0.0.0.0:8081")?
+        .bind("0.0.0.0:8080")?
         .run()
         .await
 }
